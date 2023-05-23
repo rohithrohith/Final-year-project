4a4e976f52034cec65175d3be0e338eb2ae3a4f6
rohithrohith <rur209@gmail.com>
2023-05-23 13:51:42 +0530

    Initial commit

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-export const languages = [
-    { code: "en", name: 'English' },
-    { code: "kn", name: 'Kannada' },
-    { code: "hi", name: 'Hindi' },
-    { code: "pa", name: 'Panjabi' },
-    { code: "ta", name: 'Tamil' },
-    { code: "te", name: 'Telagu' }
-]
